b094be9f1938df4fe84be5bd34a82ad40245f330
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-12-14 11:26:14 -0600

    Titulo dinamico con boton para regresar de quiz

--- id_app/app/Quiz.tsx
+++ id_app/app/Quiz.tsx
@@ -1,5 +1,6 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { RouteProp, useRoute } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 
 // Define the type for the route parameters
 type RouteParams = {
@@ -11,9 +12,21 @@ type RouteParams = {
 export default function Quiz() {
     const route = useRoute<RouteProp<{ params: RouteParams }, "params">>();
     const { item } = route.params;
+    const navigation = useNavigation();
 
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.goBack();
+                    }}
+                    style={styles.backButton}
+                >
+                    <Text>Back</Text>
+                </TouchableOpacity>
+                <Text>{item.title}</Text>
+            </View>
             <Text>Details of {item.title}</Text>
         </View>
     );
@@ -22,7 +35,19 @@ export default function Quiz() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        flexDirection: "column",
         alignItems: "center",
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#f0f0f0",
+    },
+    backButton: {
+        marginRight: 10,
+        width: 50,
+        backgroundColor: "#000",
     },
 });
--- id_app/app/_layout.tsx
+++ id_app/app/_layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout() {
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="Quiz" />
+            <Stack.Screen name="Quiz" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
         </Stack>
     );
