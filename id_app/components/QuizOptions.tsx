9db269fb0db70e9cb7c04277788f5166211162d3
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2024-12-14 16:14:45 -0600

    Simple quiz that guides you thru the options untill arriving to the result

--- id_app/components/QuizOptions.tsx
+++ id_app/components/QuizOptions.tsx
@@ -1,25 +1,22 @@
-import { View, Pressable, Text } from "react-native";
+import { View, Pressable, Text, StyleSheet } from "react-native";
+import { useState } from "react";
 
 type Props = {
     data: string;
 };
 
-/* JSON:
-[{"group": "color", "id": "0", "next": "1", "option": "Es rojo", "prev": null, "result": null}, {"group": "color", "id": "0", "next": "2", "option": "Es verde", "prev": null, "result": null}, {"group": "color", "id": "0", "next": "3", "option": "Es azul", "prev": null, "result": null}, {"group": "forma", "id": "1", "next": "4", "option": "Es cuadrado", "prev": "0", "result": null}, {"group": "forma", "id": "1", "next": "5", "option": "Es triangular", "prev": "0", "result": null}, {"group": "comida", "id": "2", "next": "6", "option": "Es fruta", "prev": "0", "result": null}, {"group": "comida", "id": "2", "next": "7", "option": "Es verdura", "prev": "0", "result": null}, {"group": null, "id": "3", "next": null, "option": null, "prev": "0", "result": "Cielo"}, {"group": null, "id": "4", "next": null, "option": null, "prev": "1", "result": "Caja"}, {"group": null, "id": "5", "next": null, "option": null, "prev": "1", "result": "Pirámide"}, {"group": null, "id": "6", "next": null, "option": null, "prev": "2", "result": "Manzana"}, {"group": null, "id": "7", "next": null, "option": null, "prev": "2", "result": "Lechuga"}]
-*/
+interface Option {
+    group?: string | null;
+    id: string;
+    next?: string | null;
+    option?: string | null;
+    prev?: string | null;
+    result?: string | null;
+    info?: string | null;
+}
 
 export default function QuizOptions({ data }: Props) {
     const jsonObject = JSON.parse(data);
-    // group the options by id
-    interface Option {
-        group: string | null;
-        id: string;
-        next: string | null;
-        option: string | null;
-        prev: string | null;
-        result: string | null;
-    }
-
     const groupedOptions = jsonObject.reduce(
         (groupedObjs: { [key: string]: Option[] }, obj: Option) => {
             const key = obj.id;
@@ -32,23 +29,53 @@ export default function QuizOptions({ data }: Props) {
         {}
     );
 
-    console.log(groupedOptions);
+    const [currentOptionId, setCurrentOptionId] = useState("0");
+    const [selectedOptions, setSelectedOptions] = useState<Option[]>([]);
+
+    const handleOptionPress = (option: Option) => {
+        setSelectedOptions([...selectedOptions, option]);
+        if (option.next) {
+            setCurrentOptionId(option.next);
+        }
+    };
 
     return (
         <View>
-            {Object.keys(groupedOptions).map((key) => {
-                return (
-                    <View key={key}>
-                        {groupedOptions[key].map((option: Option) => {
-                            return (
-                                <Pressable key={option.option}>
-                                    <Text>{option.option}</Text>
-                                </Pressable>
-                            );
-                        })}
-                    </View>
-                );
-            })}
+            {groupedOptions[currentOptionId].map(
+                (option: Option, index: number) =>
+                    option.option ? (
+                        <Pressable
+                            key={index}
+                            onPress={() => handleOptionPress(option)}
+                            style={styles.optionPressable}
+                        >
+                            <Text>{option.option}</Text>
+                        </Pressable>
+                    ) : (
+                        <Pressable
+                            key={index}
+                            onPress={() => handleOptionPress(option)}
+                            style={styles.resultPressable}
+                        >
+                            <Text>{option.result}</Text>
+                        </Pressable>
+                    )
+            )}
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    optionPressable: {
+        padding: 10,
+        margin: 5,
+        backgroundColor: "#d3d3d3",
+        borderRadius: 5,
+    },
+    resultPressable: {
+        padding: 10,
+        margin: 5,
+        backgroundColor: "#add8e6",
+        borderRadius: 5,
+    },
+});
